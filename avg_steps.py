e49aa1346b58f34a9410d8e64c23d422acad2c99
Trey <Trey_Fowler2@baylor.edu>
2022-09-08 23:10:33 -0500

    Edited avg steps to include 12th value

--- avg_steps.py
+++ avg_steps.py
@@ -13,19 +13,31 @@ writer.writerow(outfile.header)
 totalsteps=0
 month = 1
 count = 0
+accum = 1
 
 for record in csvfile:
     if int(record[0]) == month:
         count += 1
         totalsteps += int(record[1])
+
+        if int(record[0]) == 12:
+
+            month_name = calendar.month_name[accum]
+            average_steps = round(totalsteps / count, 2)
+            average_steps = str(average_steps)
+
+            outfile.write(month_name + ', ' + average_steps + '\n')
+
+            break
     #runs the sum, else statement needs to yield the average and display data
     else:
         average_steps = round((totalsteps / count), 2)
-        month_name = calendar.month_name[int(record[0])]
+        month_name = calendar.month_name[accum]
         data = [month_name, average_steps]
         writer.writerow(data)
         month = int(record[0])
         totalsteps = int(record[1])
+        accum += 1
         count = 1
 
 outfile.close()
